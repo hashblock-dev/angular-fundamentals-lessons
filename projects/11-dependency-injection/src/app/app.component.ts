cdcc020978265d54f55ed93d976a4b00e814761c
Cedric <cedric@hashblock.dev>
2025-03-22 17:31:42 +0100

    feat: add service and use DI

--- projects/11-dependency-injection/src/app/app.component.ts
+++ projects/11-dependency-injection/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { UserService } from './user.service';
+import { User } from './data';
 
 @Component({
+  imports: [UserInfoComponent],
   selector: 'app-root',
   standalone: true,
-  template: ` <h1>User Listing</h1> `,
+  template: `
+    <h1>User Listing</h1>
+    @for (user of userData; track $index) {
+    <app-user-info [user]="user" />
+    }
+  `,
 })
 export class AppComponent {
-  constructor() {}
+  userService = inject(UserService);
+  userData: User[] = [];
+
+  constructor() {
+    this.userService.getUserData().then((data) => {
+      this.userData = data;
+    });
+  }
 }
